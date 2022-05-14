531d15662ea3080b0eb82c86a12bc26b983989f3
raushankumarchaudhary <48524780+raushankumarchaudhary@users.noreply.github.com>
2022-05-14 14:08:28 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps using webhooks jhfjfgjhggkjgjgkjjkgkjgkj !");
+    System.out.println("Hello World from DevOps using webhooksjkgkjgkj !");
     }
 }
